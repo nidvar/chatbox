d2e1a77743803d6baa15fccf7a6683be9b734c3c
nidvar <djayshine87@hotmail.com>
2019-09-15 20:57:21 +1000

    total change from start

--- script.js
+++ script.js
@@ -1,74 +1,56 @@
 const div = document.getElementById('root');
 const user_input = document.getElementById('user_input');
 const form = document.getElementById('form');
-const colors=['blue', 'red', 'orange', 'green', 'black'];
-const mycss= `color:${colors[Math.floor(Math.random()*4)]}`;
-const mybackcss=`background-color:beige`;
-const new_user = `ID:${(Math.random()*100).toFixed(1)}`;
+const new_user = `(${(Math.random()*100).toFixed(0)})`;
 
-let thechats=[];
-
-let listofids=[];
+let all_id=[];
 
 
-const run=()=>{
-    const x = {
-        title:user_input.value,
-        time:new Date().getTime(),
-        user:`${document.getElementById('user_name').value} - ${new_user}`
-    }
-    //add to db
-    db.collection('items').add(x).then(()=>{console.log('added')}).catch(()=>{console.log('failed to add')})
+db.collection('items').onSnapshot((entity)=>{
+    console.log(entity.docChanges().forEach((item)=>{
+        if(item.type==='added'){
+            div.innerHTML+=`<p>${item.doc.data().user}: ${item.doc.data().title}</p>`
+            var element = document.getElementById('root');
+            element.scrollTop = element.scrollHeight - element.clientHeight;
+        }
+    }));
+})
 
-    //grab from db
-    db.collection('items').get().then((entity)=>{
-        div.innerHTML='';
-        thechats=[];
-        entity.docs.forEach((item)=>{
-            thechats.push(item.data())
-            listofids.push(item.id)
-        })
-        thechats.sort((a, b) => (a.time > b.time) ? 1 : -1);
-
-        thechats.forEach((item)=>{
-            div.innerHTML+=`
-            <div class='box' style='${mybackcss}'>
-                <div class='myflexbox'>
-                    <p><b>${item.user}:</b></p> 
-                    <p>${new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds()}</p> 
-                </div>
-                
-                <p style='${mycss}'>${item.title}</p>
-            </div>`
-        })
-
-        var element = document.getElementById('root');
-        element.scrollTop = element.scrollHeight - element.clientHeight;
-
-    }).catch(()=>{console.log('failed to grab data')});
-    user_input.value='';
+const add=(the_object)=>{
+    db.collection('items').add(the_object).then(()=>{console.log('added')}).catch(()=>{console.log('failed to add')})
 }
 
+const clear_log=()=>{
+    //clear div
+    div.innerHTML='';
+    //grab items from db
+    db.collection('items').get().then((entity)=>{
+        //push all the items - ID's - into the array above
+        entity.docs.forEach((item)=>{
+            all_id.push(item.id);
+        })
+        //for each ID in the new array, match with database and delete
+        all_id.forEach((item)=>{
+            db.collection('items').doc(item).delete().then(()=>{all_id=[];console.log('deleted')}).catch(()=>{console.log('failed to delete')})
+        })
+    }).catch(()=>{console.log('failed to get')})
+    div.innerHTML='';
+}
 
-
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    run();
-})
-
-user_input.addEventListener('keydown',(e)=>{
+user_input.addEventListener('keydown', (e)=>{
     if(e.keyCode===13){
         e.preventDefault();
-        run();
+        const x = {
+            title:user_input.value,
+            time:new Date().getTime(),
+            user:`${new_user}-<b>${user_name.value}</b>`
+        }
+        add(x);
+
         user_input.value='';
     }
-    
 })
 
-document.getElementById('clear').addEventListener('click',()=>{
-    listofids.forEach((item)=>{
-        db.collection('items').doc(item).delete()
-    })
-    div.innerHTML='';
+clear.addEventListener('click', (e)=>{
+    clear_log();
 })
-
